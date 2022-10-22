181b71d7ce4eb3d8a8defd1614147e32f5d1316d
ah.jo <ah.jo@navercorp.com>
2022-10-22 13:18:04 +0900

    Add FactoryMethodIntrospector

--- fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/property/PropertyCache.java
+++ fixture-monkey-api/src/main/java/com/navercorp/fixturemonkey/api/property/PropertyCache.java
@@ -23,8 +23,11 @@ import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.AnnotatedType;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -32,6 +35,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 import org.apiguardian.api.API;
 import org.apiguardian.api.API.Status;
@@ -50,6 +54,8 @@ public final class PropertyCache {
 	private static final Map<Class<?>, Map<String, PropertyDescriptor>> PROPERTY_DESCRIPTORS =
 		new LruCache<>(2000);
 	private static final Map<Class<?>, Map<String, Field>> FIELDS = new LruCache<>(2000);
+	private static final Map<Class<?>, Map<Method, Parameter[]>> PARAMETER_BY_FACTORY_METHOD =
+		new LruCache<>(2000);
 
 	public static List<Property> getProperties(AnnotatedType annotatedType) {
 		Map<String, List<Property>> propertiesMap = new HashMap<>();
@@ -126,6 +132,46 @@ public final class PropertyCache {
 				}
 			} catch (IntrospectionException ex) {
 				LOGGER.warn("Introspect bean property is failed. type: " + clazz, ex);
+			}
+			return result;
+		});
+	}
+
+	public static List<Property> getFactoryProperties(AnnotatedType annotatedType) {
+		List<Property> properties = new ArrayList<>();
+		Class<?> actualType = Types.getActualType(annotatedType);
+		Map<String, Field> fieldsByName = getFields(actualType);
+		Map<Method, Parameter[]> parametersByFactoryMethods = getParametersByFactoryMethods(actualType);
+
+		for (Entry<Method, Parameter[]> parametersByFactoryMethod : parametersByFactoryMethods.entrySet()) {
+			Method method = parametersByFactoryMethod.getKey();
+			Parameter[] parameters = parametersByFactoryMethod.getValue();
+			for (Parameter parameter : parameters) {
+				Field field = fieldsByName.get(parameter.getName());
+				FieldProperty fieldProperty = field != null ? new FieldProperty(field) : null;
+				properties.add(
+					new FactoryMethodProperty(
+						parameter.getAnnotatedType(),
+						method,
+						parameter.getName(),
+						fieldProperty
+					)
+				);
+			}
+			return properties;
+		}
+		return properties;
+	}
+
+	public static Map<Method, Parameter[]> getParametersByFactoryMethods(Class<?> clazz) {
+		return PARAMETER_BY_FACTORY_METHOD.computeIfAbsent(clazz, type -> {
+			Map<Method, Parameter[]> result = new ConcurrentHashMap<>();
+			List<Method> factoryMethods = Arrays.stream(type.getDeclaredMethods())
+				.filter(it -> Modifier.isStatic(it.getModifiers()) && it.getReturnType().equals(type))
+				.collect(Collectors.toList());
+
+			for (Method factoryMethod : factoryMethods) {
+				result.put(factoryMethod, factoryMethod.getParameters());
 			}
 			return result;
 		});
